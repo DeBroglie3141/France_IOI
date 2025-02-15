d39fe275cb7fd665f0f237a1d250dbe5429ad469
Jules Charlier <jules.charlier123@gmail.com>
2025-02-15 15:46:43 +0100

    3 nouveaux exs CF

--- A_Next_Round.py
+++ A_Next_Round.py
@@ -1,3 +1,11 @@
-n, k = map(int, input().split())
+_, k = map(int, input().split())
 scores = list(map(int, input().split()))
+limite = scores[k-1]
+n = 0
 
+for score in scores:
+    if (score > 0) and (score >= limite):
+        n += 1
+    else:
+        pass
+print(n)
